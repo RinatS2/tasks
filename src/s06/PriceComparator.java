e8242d033995248e52bf0e02dfd8add34c3e1048
Rinat Suleimanov <rinravis@gmail.com>
2024-08-18 20:40:07 +0300

    Task06

---
+++ src/s06/PriceComparator.java
@@ -0,0 +1,11 @@
+package s06;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+
+public class PriceComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product product1, Product product2) {
+        return Double.compare(product1.getPrice(), product2.getPrice());
+    }
+}
