40f08389bbdeaecb9cd749ada7310cf31beacd6f
Rinat Suleimanov <rinravis@gmail.com>
2024-08-18 21:54:47 +0300

    Task09

--- src/s09/Task01.java
+++ src/s09/Task01.java
@@ -4,5 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Task01 {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
 
+        stringList.add("A");
+        stringList.add("B");
+        stringList.add("C");
+        stringList.add("D");
+        stringList.add("E");
+
+        System.out.println(nameSearch("u", stringList));
+    }
+
+    public static boolean nameSearch(String searchName, List<String> nameList) {
+        boolean b = false;
+        for (String s : nameList) {
+            if (searchName.equals(s)) {
+                return true;
+            }
+        }
+        return b;
+    }
 }
